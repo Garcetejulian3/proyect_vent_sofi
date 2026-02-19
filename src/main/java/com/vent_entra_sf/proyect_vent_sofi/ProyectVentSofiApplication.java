e7335b5e60c504a99bc82d41a4f0f5ba6648b2ee
garcetejulian3@gmail.com <garcetejulian3@gmail.com>
2026-02-18 21:15:15 -0300

    subiendo proyecto base

---
+++ src/main/java/com/vent_entra_sf/proyect_vent_sofi/ProyectVentSofiApplication.java
@@ -0,0 +1,13 @@
+package com.vent_entra_sf.proyect_vent_sofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectVentSofiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectVentSofiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vent_entra_sf/proyect_vent_sofi/ProyectVentSofiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vent_entra_sf.proyect_vent_sofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectVentSofiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
